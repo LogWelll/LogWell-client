4abb1e6e7abb2c014176d9f922613bb35f270117
amirberernji <ahberenji@gmail.com>
2025-08-20 14:10:21 +0330

    initial commit

---
+++ example.py
@@ -0,0 +1,17 @@
+import logging
+from src.handler import LogServiceHandler
+from src.schema import Endpoint
+
+# Configure root logger
+logger = logging.getLogger("my-app")
+logger.setLevel(logging.DEBUG)
+
+# Add your custom handler
+handler = LogServiceHandler(base_url="http://localhost:8000", api_key="key1")
+logger.addHandler(handler)
+
+# Standard logging works as usual
+logger.info(
+    "Hello world!", extra={"tag": "tag1", "endpoint": Endpoint.NonBlocking}
+)  # will be sent via your microservice
+logger.error("Something went wrong", extra={"tenant": "customerA"})
